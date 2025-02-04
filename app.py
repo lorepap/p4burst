69386842258668c7a90a16d6f8f3e7b1fb2cd8c6
lorepap <p.lore96@gmail.com>
2025-02-04 16:20:01 +0000

    fix app log path

--- app.py
+++ app.py
@@ -15,13 +15,15 @@ import numpy as np
 
 
 class App(ABC):
-    def __init__(self, mode, log=True):
-        self.mode = mode
+    def __init__(self, args):
+        self.mode = args.mode
         self.client = None
         self.server = None
         self.config = self.load_config('config.ini')
-        if log:
-            self.setup_logging()
+        if not args.disable_logging:
+            if not os.path.exists(f'tmp/{args.exp_id}'):
+                os.makedirs(f'tmp/{args.exp_id}')
+            self.setup_logging(log_file=f'tmp/{args.exp_id}/app.log')
 
     @abstractmethod
     def run(self):
@@ -51,19 +53,19 @@ class App(ABC):
 
 class BurstyApp(App):
     def __init__(self, args):
+        super().__init__(args)
         mode = args.mode
         host_ip = args.host_ip
         server_ips = args.server_ips
         reply_size = args.reply_size
         incast_scale = args.incast_scale
         qps = args.qps
-        super().__init__(mode)
         if mode == 'server':
             self.server = BurstyServer(reply_size=reply_size, ip=args.host_ip, exp_id=args.exp_id)
         elif mode == 'client':
             if server_ips is None:
                 raise ValueError("server_ips must be provided for client mode")
-            self.client = BurstyClient(server_ips, reply_size, qps=qps, incast_scale=incast_scale, exp_id=args.exp_id)
+            self.client = BurstyClient(server_ips, reply_size, qps=qps, exp_id=args.exp_id)
         else:
             raise ValueError("Invalid mode. Choose 'server' or 'client'.")
 
@@ -75,7 +77,7 @@ class BurstyApp(App):
 
 class BackgroundApp(App):
     def __init__(self, args):
-        super().__init__(args.mode)
+        super().__init__(args)
         mode = args.mode
         # host_id = args.host_id
         host_ip = args.host_ip
@@ -109,10 +111,10 @@ class BackgroundApp(App):
 
 class SimplePacketApp(App):
     def __init__(self, args):
+        super().__init__(args)
         mode = args.mode
         server_ip = args.server_ips
         # packet_size = args.packet_size
-        super().__init__(mode)
         if mode == 'server':
             self.server = BaseServer(port=12345, ip=args.host_ip, exp_id=args.exp_id)  # Use a basic server listener
         elif mode == 'client':
@@ -130,9 +132,9 @@ class SimplePacketApp(App):
 
 class IperfApp(App):
     def __init__(self, args):
+        super().__init__(args)
         mode = args.mode
         server_ip = args.server_ips
-        super().__init__(mode)
         if mode == 'server':
             self.server = IperfServer(port=12345, ip=args.host_ip)  # Use a basic server listener
         elif mode == 'client':
@@ -154,6 +156,7 @@ def main():
     parser.add_argument('--mode', choices=['server', 'client'], required=True)
     parser.add_argument('--type', choices=['bursty', 'background', 'single'], required=True)
     parser.add_argument('--host_ip', type=str, required=False, help="host IP address (required for servers)")
+    parser.add_argument('--disable_logging', action='store_true', help="Disable logging")
     # parser.add_argument('--host_id', type=str, required=False, help="sender id: required only for a sender")
     parser.add_argument('--server_ips', required=False, nargs='+', help="List of server IPs (bursty app)")
     parser.add_argument('--reply_size', required=False, type=int, default=40000)
