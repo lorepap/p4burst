eec1743059b1eafac5e39af99c61609c20cccf33
lorepap <p.lore96@gmail.com>
2025-03-14 12:20:12 +0000

    Add queue depth to bee packets

--- utils/bee_packets.py
+++ utils/bee_packets.py
@@ -1,18 +1,20 @@
-from scapy.all import Ether, IP, UDP, Packet, BitField, sendp
+from scapy.all import Ether, IP, UDP, Packet, BitField, sendp, bind_layers
 
 class BeeHeader(Packet):
     name = "BeeHeader"
     fields_desc = [
-        BitField("port_idx_in_reg", 0, 31),
-        BitField("queue_occ_info", 0, 1)
+        BitField("port_idx_in_reg", 0, 28),
+        BitField("queue_occ_info", 0, 1),
+        BitField("queue_depth", 0, 19) 
     ]
-
+    
 def send_bee_packets(switch: str):
     switch_name = switch
     iface = switch_name + '-eth1'
     for port in range(8):
-        pkt = (Ether() / 
-                IP(src="0.0.0.0", dst="0.0.0.0") /  # Minimal IP header
+        pkt = (Ether() /  # Use broadcast MAC
+                IP(src="0.0.0.0", dst="0.0.0.0") /
                 UDP(dport=9999) /
-                BeeHeader(port_idx_in_reg=port))
+                BeeHeader(port_idx_in_reg=port, ))
         sendp(pkt, iface=iface, verbose=False)
+
